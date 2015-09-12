065d4a4715a49ef8b4b91c4edf8b65ce167938b8
Ankit s <ankit.s@flipkart.com>
2015-09-12 16:23:20 +0530

    Adds servers array and response formatting

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-//Lets require/import the HTTP module
+//require/import the HTTP module
 var http = require('http'),
     request = require('request'),
     _ = require('lodash');
 
-//Lets define a port we want to listen to
+//define a port we want to listen to
 const PORT=5000;
+
+//define host addresses
+//var address = ["http://localhost:25280", "http://sp-cms-service36.nm.flipkart.com:26700/sp-cms-service/spService?api=productDWL&ids=KTAE2FK9H7AFZBHX", "http://sp-cms-service36.nm.flipkart.com:26700/sp-cms-service/spService?api=productDWL&ids=BOLE9GZ6GFVT3YMK"]
+
+var address = ["http://sherlock-app22.nm.flipkart.com:25280/sherlock/stores/tyy/4io/select?q=i%20phone"];
+
+// This function generates random integer between two numbers low (inclusive) and high (exclusive) ([low, high))
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
 
 //We need a function which handles requests and send response
 function handleRequest(req, response){
 
     console.log(req);
 
-
-    console.log(req.method);
-    console.log(req.params);
-    console.log(req.body);
-    console.log(req.query);
-    var requestOptions = {
-        method : req.method,
-        url : "http://www.google.com/"
-    };
-    req.url = 'http://www.google.com';
-
+    var rand = randomInt(0, address.length);
+    console.log(rand);
+    req.url = address[rand];//"http://localhost:25280"
     request(req, function(err, res, body) {
         if (err) {
             throw err;
         }
-        response.end(body);
+        console.log(res);
+        response.end(JSON.parse(JSON.stringify(res)));
     });
-
-
-
-    //response.end('It Works!! Path Hit: ' + request.url);
 }
 
 //Create a server
