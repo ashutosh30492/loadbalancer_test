a4cad5002a1716ca092ecf1360ad091f9ce269f2
ASHUTOSH SHARMA <ashutosh30492@gmail.com>
2015-09-14 03:07:15 +0530

    Adds jshintrc and jscsrs. Fixes jshint errors.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var http = require('http'),
-    httpProxy = require('http-proxy');
+    httpProxy = require('http-proxy'),
+    address;
 
-//
-// Create a proxy server with custom application logic
-//
+//Initialise server array
 
-var address = ["http://www.google.com", "http://www.flipkart.com", "http://stayzilla.com", "http://photosapi.qa.stayzilla.com/pictures/hotel"];
+address = ['http://www.google.com', 'http://www.flipkart.com', 'http://stayzilla.com', 'http://photosapi.qa.stayzilla.com/pictures/hotel'];
 
 /**
  *This function generates random integer between two numbers low (inclusive) and high (exclusive) ([low, high))
@@ -13,22 +12,20 @@ var address = ["http://www.google.com", "http://www.flipkart.com", "http://stayz
  * @param high
  * @returns {number}
  */
-function randomInt (low, high) {
+function randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
 
-var proxy = httpProxy.createProxyServer({});
-
-var server = http.createServer(function(req, res) {
-
-    var target = address[randomInt(0, address.length)];
-
-    delete req.headers.host;
-    console.log("Redirected to : " + target);
-    proxy.web(req, res, {
-        target: target
+var proxy = httpProxy.createProxyServer({}),
+    target,
+    server = http.createServer(function (req, res) {
+        target = address[randomInt(0, address.length)];
+        delete req.headers.host;
+        console.log('Redirected to : ' + target);
+        proxy.web(req, res, {
+            target: target
+        });
     });
-});
 
-console.log("listening on port 5050");
+console.log('Load balancer listening on port 5050');
 server.listen(5050);
